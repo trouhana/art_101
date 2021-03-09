99ccbc08f9f0a6fd8b3ac63a8d8d82e5b33656f2
trouhana <trouhana@users.noreply.github.com>
2021-03-09 13:24:08 -0800

    Update ajaxFunction.js

--- sectionC/js/ajaxFunction.js
+++ sectionC/js/ajaxFunction.js
@@ -1,21 +1,44 @@
 function getAjax(){
-$.ajax({
-  url:"https://en.wikipedia.org/api/rest_v1/page/title/Warren_G._Harding",
-  type:"GET",
-  dataType:"json",
-  success: successFun,
-  error:errorFun,
-  complete: function (xhr, staus){
+ $.ajax({
+  "async": true,
+ 	"crossDomain": true,
+  url:"https://genius.p.rapidapi.com/artists/16775/songs",
+   type:"GET",
+   dataType:"json",
+   headers: {
+ 		"x-rapidapi-key": "77f9d9ff88msh9bc93d96fb25dacp1b7291jsn5be61e72cb6e",
+ 		"x-rapidapi-host": "genius.p.rapidapi.com"
+ 	},
+   success: successFun,
+   error:errorFun,
+   complete: function (xhr, staus){
     console.log("request is complete"+xhr);
-  }
+   }
 
-});
-}
-function successFun(result){
-  console.log(result);
-  $("#ajax_text").html(result.items[0].title);
-}
-function errorFun(xhr, status, strErr){
-  console.log("error "+strErr);
-}
+ });
+ }
+ function successFun(result){
+   console.log(result);
+   $("#ajax_text").html('<img src="'+result.response.songs[10].song_art_image_url+'"/>"')
+   //$("#ajax_text").html(result.items[0].title);
+ }
+ function errorFun(xhr, status, strErr){
+   console.log("error "+strErr);
+ }
+// const settings = {
+// 	"async": true,
+// 	"crossDomain": true,
+// 	"url": "https://genius.p.rapidapi.com/artists/16775/songs",
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-key": "77f9d9ff88msh9bc93d96fb25dacp1b7291jsn5be61e72cb6e",
+// 		"x-rapidapi-host": "genius.p.rapidapi.com"
+// 	}
+// };
+//
+// $.ajax(settings).done(function (response) {
+// 	console.log(response);
+//   $("#ajax_text").html('<img src="'+response.response.songs[10].song_art_image_url+'"/>"')
+// });
+//}
 $("#click_ajax").click(getAjax);
