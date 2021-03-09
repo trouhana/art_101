064ec813a4a7ad57b2fd8fc98e35d8302df25b99
trouhana <trouhana@users.noreply.github.com>
2021-03-09 11:52:32 -0800

    Update ajaxFunction.js

--- sectionC/js/ajaxFunction.js
+++ sectionC/js/ajaxFunction.js
@@ -1,19 +1,19 @@
 function getAjax(){
 $.ajax({
-  url:"https://xkcd.com/info.0.json",
+  url:"https://en.wikipedia.org/api/rest_v1/page/title/Warren_G._Harding",
   type:"GET",
   dataType:"json",
   success: successFun,
   error:errorFun,
   complete: function (xhr, staus){
     console.log("request is complete"+xhr);
-  }
+  
 
 });
 }
 function successFun(result){
   console.log(result);
-  $("#ajax_text").html(result.img);
+  $("#ajax_text").html(result.items[0].title);
 }
 function errorFun(xhr, status, strErr){
   console.log("error "+strErr);
